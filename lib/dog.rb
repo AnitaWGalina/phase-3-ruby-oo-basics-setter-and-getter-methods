ef4427e4731294591dda870bb15625ad1db78cc6
anitagalina <anitagalina21@gmail.com>
2023-05-25 11:15:59 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
+  def name=(name)
+    @name=name
 end
+
+sunshine = Dog.new
+sunshine.name = "Sunshine"
+
+
+
+class Dog
+    def breed=(breed)
+        @breed=breed
+    end
+
+husky = breed.new
+husky.breed = "Husky"
+
+   
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+  def name =(name)
+    @name=name
 
 end
+
+
+anita=Person.new
+anita.name = "Anita"
+
+
+class Person
+    def job=(job)
+        @job=job
+
+    end
+
+doctor=Person.new
+doctor.job="Doctor"
